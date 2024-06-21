e1d2da45709a0dbe2a62f879d589908e53e1ea3c
yunTum <yusukeaonuma616@gmail.com>
2024-06-21 15:38:11 +0900

    angluar startup

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './main/login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ]
+})
+export class AppModule { }
---
+++ src/app/main/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.less'
+})
+export class LoginComponent {
+  public title = 'login'
+}
